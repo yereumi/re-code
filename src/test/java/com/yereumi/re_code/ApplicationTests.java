a983b088454cdaef670999bcf47c14a9e17dadc5
yereumi <lovelyyeji01@gmail.com>
2026-02-13 18:51:46 +0900

    test: Testcontainers 기반 테스트 환경 추가 (#2)

--- src/test/java/com/yereumi/re_code/ApplicationTests.java
+++ src/test/java/com/yereumi/re_code/ApplicationTests.java
@@ -2,12 +2,22 @@ package com.yereumi.re_code;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers(disabledWithoutDocker = true)
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:latest");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
